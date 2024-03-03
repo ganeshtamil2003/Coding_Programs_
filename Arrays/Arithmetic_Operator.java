7bb8baa786be4052844a232a1ed2cf82cdae0b14
ganeshtamil2003 <ganesh28062003@gmail.com>
2024-03-04 00:11:18 +0530

    java code

---
+++ Arrays/Arithmetic_Operator.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Arithmetic_Operator{
+    public static void main(String args[]){
+        int a=25, b=10;
+        float c=25.5f, d=4 ;
+        System.out.println("a ="+a); 
+        System.out.println("b = " + b); 
+        System.out.println("c = " + c); 
+        System.out.println("d = "+d);
+        System.out.println(" a + b = " + ( a + b ) );
+        System.out.println( "a - b = "+(ab));
+        System.out.println( "a * b = " + (a*b) );
+        System.out.println( "a / b = " + (a/b)); 
+        System.out.println("a % b = " + (a%b));
+        System.out.println(" a + c = " + ( a + c ) ); 
+        System.out.println( "c + d = " + (d+d));
+        System.out.println( "c % d = " + (c%d));
+        System.out.println("-a % b = " + (-a%b)); 
+        System.out.println( "-a % -b = " + (-a%-b)); 
+        System.out.println("a % b= "+(a%b));
+        System.out.println("a % -b = " + (a%-b));
+    }
+}
