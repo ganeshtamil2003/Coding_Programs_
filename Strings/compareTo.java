d72abee04dba55e892670f461788bec6a950b948
ganeshtamil2003 <ganesh28062003@gmail.com>
2024-03-08 23:58:37 +0530

    compareTo

---
+++ Strings/compareTo.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class compareTo.java{
+    public static void main(String[] args) {
+        String s1="JAVA";
+        String s2="JAVA";
+        String s3="java";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+}
+}
