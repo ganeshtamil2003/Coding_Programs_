79db16be478c6365e94e9e886106bc36b2c2ea39
ganeshtamil2003 <ganesh28062003@gmail.com>
2024-03-28 07:37:30 +0530

    code added

--- Patterns/patternsPractice.java
+++ Patterns/patternsPractice.java
@@ -1,6 +1,6 @@
 class patternsPractice{
     public static void main(String args[]){
-        pattern6(5);
+        pattern30(5);
     }
     static void pattern1(int n){
         for(int row=1;row<=n;row++){
@@ -69,4 +69,20 @@ class patternsPractice{
             System.out.println();
         }
     } 
+
+    static void pattern30(int n){
+    for(int row=1;row<=n;row++){
+        for(int s=0;s<n-row;s++){
+            System.out.print("  ");
+        }
+        for(int col=row;col>=1;col--){
+            System.out.print(col+" ");
+        }
+        for(int col=2;col<=row;col++){
+            System.out.print(col+" ");
+        }
+        System.out.println();
+    }
 }
+}
+
